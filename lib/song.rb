2686709c136d684f2551babf44c5a5cbae555c82
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-06-14 20:54:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,17 @@ class Song
     end
     genre_count
   end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #if the artist is already in the hash
+        artist_count[artist] += 1
+      else #if the artist isn't already in the hash
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
 end
